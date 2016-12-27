495fd8dcd6ebc9943dd5aac01cf11b5b86cabe9a
rohman123 <rohman.telesandi12@gmail.com>
2016-12-27 22:37:42 +0700

    Add files via upload

---
+++ sosial.php
@@ -0,0 +1,8 @@
+<div class="sosial">
+        <p id="sss">
+            <a href="https://web.facebook.com/skay.Tls"><img border="0" width="50" height="50" title="facebook" src="img/kontak/facebook.png" /></a>
+                 <a href="https://www.instagram.com/rohmannurhaqiqi/"><img border="0" width="50" height="50" title="instagram" src="img/kontak/instagram.png" /></a>
+                   <a href="https://twitter.com/Rohman_NH"><img border="0" width="50" height="50" title="twitter" src="img/kontak/twitter.png" /></a>
+                     <a href="https://plus.google.com/u/0/113351799737568089888"><img border="0" width="50" height="50" title="google-plus" src="img/kontak/google-plus.png" /></a>
+                  </p>
+      </div>
